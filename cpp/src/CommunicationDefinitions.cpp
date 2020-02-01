2c8cd420ed9485d354c21161a1abff96d60b19e8
Justin Newberry <jnewb@umich.edu>
2020-02-01 18:34:33 +0000

    added more types

--- cpp/src/CommunicationDefinitions.cpp
+++ cpp/src/CommunicationDefinitions.cpp
@@ -3,17 +3,18 @@
 using namespace comm;
 
 const std::map<CommunicationDefinitions::TYPE, int> CommunicationDefinitions::PACKET_SIZES = {
-            {TYPE::JOYSTICK,  127},
-            {TYPE::VISION,   127},
+            {TYPE::JOYSTICK,  8},
+            {TYPE::VISION,   36},
             {TYPE::REALSENSE,  127},
             {TYPE::DATAAGGREGATOR_STATE,  127},
-            {TYPE::DASHBOARD_DATA,  127},
-            {TYPE::ROBOT_STATE,  230},
+            {TYPE::DASHBOARD_DATA,  9},
+            {TYPE::SENSOR_STATE,  120},
 
             {TYPE::VISION_COMMAND,  127},
             {TYPE::VISION_IMAGE,  8191},
             {TYPE::IDENTIFIER,  127},
-            {TYPE::VISION_PROPERTIES, 127}
+            {TYPE::VISION_PROPERTIES, 127},
+            {TYPE::ROBOT_STATE, 1}
         };
 
 char CommunicationDefinitions::key[3] = { 44, 254, 153 };
